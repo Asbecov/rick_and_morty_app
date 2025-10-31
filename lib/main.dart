f8448d7fe563c95e60674dbf0006329f4dd7749b
Asbecov <m2311505@edu.misis.ru>
2025-10-31 15:10:12 +0300

    feature: implement basic project structure

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'package:rick_and_morty_app/rick_and_morty_app/app.dart';
+
 void main() {
-  runApp(const MainApp());
-}
-
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
-    );
-  }
+  runApp(const RickAndMortyApp());
 }
---
+++ lib/rick_and_morty_app/app.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class RickAndMortyApp extends StatelessWidget {
+  const RickAndMortyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) =>
+      MaterialApp.router(title: "Rick And Morty App");
+}
